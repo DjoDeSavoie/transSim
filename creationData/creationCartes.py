6ba538e9354aa4a98c36c3acf1a3fbe10a42204e
DjoDeSavoie <jonathandegrange123@gmail.com>
2024-01-24 22:05:12 +0100

    Update creationCartes.py

--- creationData/creationCartes.py
+++ creationData/creationCartes.py
@@ -4,6 +4,4 @@ conn = pymysql.connect(user ='root', password='', host='localhost', database='tr
 cursor = conn.cursor()
 
 
-# queryCreateCarte = "INSERT INTO cartebancaire (numeroCarte, idCompteEmetteur, dateExpiration, validite, pin, cryptogramme) VALUE (%s, %s, %s, %s, %s, %s)"
-# r = cursor.execute(queryCreateCarte, ('1234567891234567', '1', '2020-12-31', '1', '1234', '123'))
 
