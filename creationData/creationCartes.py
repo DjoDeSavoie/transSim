019031fb499ea0378554476b45edc0ddebee504f
DjoDeSavoie <jonathandegrange123@gmail.com>
2024-01-25 07:13:24 +0100

    Update creationCartes.py

--- creationData/creationCartes.py
+++ creationData/creationCartes.py
@@ -4,4 +4,6 @@ conn = pymysql.connect(user ='root', password='', host='localhost', database='tr
 cursor = conn.cursor()
 
 
+#création des cartes bancaires associées aux comptes acquéreurs
 
+#savoir si : on crée une carte par acquéreur ou plusieurs, si on le fait automatiquement après la création du compte en banque ou si on le fait manuellement
