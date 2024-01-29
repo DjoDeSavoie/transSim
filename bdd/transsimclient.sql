d1632585c927f47fff50514305f33fdd4ea27b17
DjoDeSavoie <jonathandegrange123@gmail.com>
2024-01-29 09:18:02 +0100

    nomenclature

--- bdd/transsimclient.sql
+++ bdd/transsimclient.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 23 jan. 2024 à 16:24
+-- Généré le : lun. 29 jan. 2024 à 08:15
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -48,7 +48,20 @@ CREATE TABLE IF NOT EXISTS `banque` (
   `idBanque` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identifiant de la banque, clé primaire',
   `nomBanque` varchar(128) DEFAULT NULL COMMENT 'nom de la banque',
   PRIMARY KEY (`idBanque`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1164 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `banque`
+--
+
+INSERT INTO `banque` (`idBanque`, `nomBanque`) VALUES
+(1, 'creditMutuel'),
+(2, 'banquePostale'),
+(3, 'lcl'),
+(4, 'societeGenerale'),
+(5, 'bnp'),
+(6, 'caisseEpargne'),
+(7, 'creditAgricole');
 
 -- --------------------------------------------------------
 
@@ -81,10 +94,19 @@ CREATE TABLE IF NOT EXISTS `comptebancaireacquereur` (
   `numeroCompte` int UNSIGNED NOT NULL,
   `nom` varchar(128) NOT NULL,
   `prenom` varchar(128) NOT NULL,
-  `soldeCompteAcquereur` int NOT NULL,
+  `soldeCompteAcquereur` int DEFAULT '0',
   PRIMARY KEY (`idCompteAcquereur`),
   KEY `fk_banque_comptebancaireacquereur` (`idBanqueAcquereur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `comptebancaireacquereur`
+--
+
+INSERT INTO `comptebancaireacquereur` (`idCompteAcquereur`, `idBanqueAcquereur`, `numeroCompte`, `nom`, `prenom`, `soldeCompteAcquereur`) VALUES
+(4, 1, 0, 'Degrange', 'Jonathan', 0),
+(5, 7, 0, 'Issad', 'Aris', 0),
+(6, 4, 0, 'Bop', 'Falilou', 0);
 
 -- --------------------------------------------------------
 
